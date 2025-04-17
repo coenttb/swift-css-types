4d231fb5f27f34e97b0917e65ad8235922c1db39
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-04-17 11:45:25 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -95,6 +95,7 @@ let package = Package(
             dependencies: [
                 .types,
                 .properties,
+                .atRules,
             ]
         ),
         .testTarget(
