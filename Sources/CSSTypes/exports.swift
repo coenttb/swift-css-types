25739dc880046bd8f68161ea87480c14b4cd544f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-22 22:19:39 +0200

    wip

--- Sources/CSSTypes/exports.swift
+++ Sources/CSSTypes/exports.swift
@@ -5,7 +5,12 @@
 //  Created by Coen ten Thije Boonkkamp on 27/03/2025.
 //
 
+@_exported import CSSAtRuleTypes
+@_exported import CSSCombinatorTypes
+@_exported import CSSFunctionTypes
+@_exported import CSSPseudoClassTypes
+@_exported import CSSPseudoElementTypes
+@_exported import CSSSelectorTypes
 @_exported import CSSTypeTypes
 @_exported import CSSPropertyTypes
-@_exported import CSSAtRuleTypes
 
